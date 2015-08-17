d0fcf1863a4e3f10865aaae957e28a91f0c7afca
RawK <farraw33@users.noreply.github.com>
2015-08-16 21:39:47 -0700

    updated the while loop and functions
    Changed the while loop to a do/while loop and added 2 functions. Currently stops after user chooses letter placement position.

--- game/Project2.java
+++ game/Project2.java
@@ -3,8 +3,10 @@ package game;
 import java.io.*;
 import java.util.Scanner;
 
-public class Project2 {
-
+public class Project2 
+{
+	private static Scanner scan = new Scanner(System.in); //Scanner to be used in any method
+	
 	public static void main(String[] args) 
 	{
 		char letter; //used for box input
@@ -17,36 +19,63 @@ public class Project2 {
 		};
 
 		//display table
-		displayTable(table);
+	//	displayTable(table);
 
 		//prompt user to pick their piece
 		System.out.println("Do you want to play with X or O? ");
 
-		//user's choice of character
-		Scanner scan = new Scanner(System.in);		
-		userChar = scan.next();
-
 		//validate the user's choice input
-		while(userChar != "")
+		do
 		{
+			//user's choice of character
+			userChar = scan.next();
+				
 			if(userChar.equalsIgnoreCase("X"))
 			{
 				playingChar = "X";
 				computerChar = "O";
+				
+				chooseMove(playingChar);
+				charPosition = scan.next();
 			}
 			else if(userChar.equalsIgnoreCase("O"))
 			{
 				playingChar = "O";
 				computerChar = "X";
+				
+				chooseMove(playingChar);
+				charPosition = scan.next();
 			}
 			else
 			{
-				// attempt to read letter again
-				System.out.println("You can only choose X or O... what's it gonna be?!");
-				userChar = scan.next();
+				System.out.println("You have entered an invalid letter. Please select either X or O: ");
 			}
-		}
+					
+		} while(!(userChar.equalsIgnoreCase("X") || userChar.equalsIgnoreCase("O")));
 
+	}
+	
+	public static void chooseMove(String charChoice)
+	{
+		System.out.println("\nWhere would you like to place your " + charChoice + "?");
+		System.out.println("(choose position from the table displayed below. Example: A1) \n");
+		displayInitialTable();	
+		System.out.println("Position: ");
+	}
+	
+	public static void displayInitialTable()
+	{
+		//sample table. (Update: include letter in table)  --> Alex displayTable(char box[][])!
+		String row1 = "     1     2     3   \n";
+		String row2 = "A |     |     |     |\n";
+		String row3 = "B |     |     |     |\n";
+		String row4 = "C |     |     |     |\n";
+		String rowB = "  ------------------- \n";
+		
+		String table = row1 + rowB + row2 + rowB + row3 + rowB + row4 + rowB;
+		
+		//sample table display. 
+		System.out.println(table);
 	}
 
 	//IN PROGRESS
